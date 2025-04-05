7ee2991356b2f3935197fe5473733728542df951
Gabriel Lucena <gabriellucena23@gmail.com>
2025-04-05 12:59:17 -0300

    Created project

---
+++ SandboxUnreal/Source/SandboxUnreal/SandboxUnrealGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SandboxUnrealGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASandboxUnrealGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASandboxUnrealGameMode();
+};
+
+
+
